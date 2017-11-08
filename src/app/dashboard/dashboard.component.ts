77fc26efb62e53d44c6cc852185de17b8a542423
Dat Le <dat.le@sap.com>
2017-11-08 16:58:04 +0700

    Added drag & drop sample code.
    Change-Id: Id680aedd29586c5def6d685eec08aef485bbf65b

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +11,24 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  allowDrop(event) {
+    event.preventDefault();
+  }
+
+  drop(event) {
+    event.preventDefault();
+    var domId = event.dataTransfer.getData("id"),
+      target = event.target,
+      dashboardColumn = $(target).parents("div.card.dashboard-column"),
+      columnBody = dashboardColumn.children("div.card-body");
+
+    columnBody.append(document.getElementById(domId));
+  }
+
+  drag(event) {
+    event.dataTransfer.setData("id", event.target.id);
   }
 
 }
